8e4a0c59de208a10ac1c3d99e959e12822637496
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-21 03:51:20 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,11 @@
 class Post
-  attr_accessor :title
+  attr_accessor :title, :author
 
+  def initialize(title)
+    @title = title
+  end
+
+  def author_title
+    self.author.title ? self.author.title : nil
+  end
 end

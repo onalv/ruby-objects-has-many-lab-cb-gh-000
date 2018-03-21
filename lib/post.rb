f22f1c7fb3274bb992754c5363c7ea3ff79bfc22
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-21 03:52:12 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -5,7 +5,7 @@ class Post
     @title = title
   end
 
-  def author_title
-    self.author.title ? self.author.title : nil
+  def author_name
+    self.author.name ? self.author.name : nil
   end
 end
